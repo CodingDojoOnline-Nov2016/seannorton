ec0b6e5caa06d18697bb64357e625b2862b473b4
Sean Norton <seannorton0807@gmail.com>
2016-11-17 14:29:19 -0600

    algorithm homework november 17th and changes on nov16th hw.

--- algorithmHW/Nov16HW.js
+++ algorithmHW/Nov16HW.js
@@ -15,9 +15,9 @@ function twelvebb() {
 twelvebb();
 
 //Fibonacci
-function fib(x) {
+function nth_fib(x) {
   if (x === 0) {//0 is "given"
-    return 0
+    return 0;
   }
   if (x === 1) {//1 is "given"
     return 1;
@@ -25,7 +25,7 @@ function fib(x) {
   var a = 0;
   var b = 1;
   var temp = 0;
-  while (x-2 >= 0) {//x is the index number... but we subtract 2 becuse the first two are "given"
+  while (x-2 >= 0) {//x is the index number... but we subtract 2 becuse the first two are "given" //x-2 reevaluates every time here. (can just do x -= 2 earlier.) //can also just do while (x) because 0 is false.
     temp = a + b;
     a = b;
     b = temp;
@@ -34,8 +34,20 @@ function fib(x) {
   return b;
 }
 
-console.log(fib(0));
-console.log(fib(1));
-console.log(fib(2));
-console.log(fib(3));
-console.log(fib(7));
+console.log(nth_fib(0));
+console.log(nth_fib(1));
+console.log(nth_fib(2));
+console.log(nth_fib(3));
+console.log(nth_fib(4));
+console.log(nth_fib(5));
+console.log(nth_fib(7));
+
+  // 0 1 1
+  // a b c
+  //   a b
+
+  //do it recursively.
+
+  function recurFib(n) {
+
+  }
---
+++ algorithmHW/Nov17HW.js
@@ -0,0 +1,142 @@
+//SumToOne
+//keep adding the digits in a number to get a single number.
+
+// function sumToOne(num) {
+//   if (num.length===1) {
+//     return num
+//   }
+//   var i = 0;
+//   var sum = 0;
+//   while (i < num.length) {
+//     sum += num[i];//Doesn't work because you can't index a number?
+//     i++;
+//   }
+//   sumToOne(sum)
+// }
+
+// console.log(sumToOne(928));
+
+// function digitsadd(num) {
+//   var i = 0;
+//   var sum = 0;
+//   while (i < num.length) {
+//     sum += num[i];
+//     i++;
+//   }
+//   return sum;
+// }
+//
+// console.log(digitsadd(1000));
+
+// var num = 928;
+// console.log(num[1]);//I can't get the index of a number?
+
+function sumToOne2(num) {
+  if (num.length === 1) {
+    return num;
+  }
+  var numstring = num.toString(10); //This doesn't work?
+  var i = 0;
+  var sum = 0;
+  for (var i = 0; i < numstring.length; i++) {
+    sum += parseInt(numstring[i]);
+  }
+  sumToOne2(sum);
+}
+
+// console.log(sumToOne2(928));
+
+function sumDigits(num) {
+  var numstring = num.toString(10);
+  var sum = 0;
+  for (var i = 0; i < numstring.length; i++) {
+    sum += parseInt(numstring[i]);
+  }
+  return sum;
+}
+
+console.log("sumDigits(928) = "+sumDigits(928));
+
+function sumDigitsWhile(num) {
+  var numstring = num.toString(10);
+  var sum = 0;
+  var i = 0;
+  while (i < numstring.length) {
+    sum += parseInt(numstring[i]);
+    i++;
+  }
+  return sum
+}
+
+console.log("sumDigitsWhile(928) = "+sumDigitsWhile(928));//IT WORKS NOW MAKE IT RECURSIVE. GIVES BACK 19
+
+function sumToOne3(num) {
+  if (num.length === 1) {
+    return num;
+  }
+  var numstring = num.toString(10);
+  var sum = 0;
+  var i = 0;
+  while (i < numstring.length) {
+    sum += parseInt(numstring[i]);
+    i++;
+  }
+  return sum
+}
+
+//console.log(sumToOne3(1));//GIVES BACK 1
+//console.log(sumToOne3(928));//THIS WORKS TOO! NOW I NEED TO MAKE IT LOOP. GIVES BACK 19
+
+function sumToOne4(num) {
+  var numstring = num.toString(10);
+  var sum = 0;
+  var i = 0;
+  while (i < numstring.length) {
+    sum += parseInt(numstring[i]);
+    i++
+  }
+  if (sum.toString().length > 1) {
+    return sumToOne4(sum);
+  }
+  else {
+    return sum;
+  }
+}
+
+console.log("sumToOne4(928) = "+sumToOne4(928));
+console.log("sumToOne4(25) = "+sumToOne4(25));
+
+//********************************************************************//
+
+//Clock Hand Angles
+//given a time input of xxxxxx (hours, minutes, seconds), print the angles as such:
+//Hour hand: 30 degs. Minute hand: 0 degs. Second hand: 0 degs.
+
+function clockHandAngles(seconds) {
+  //convert seconds to hours minutes seconds separately... then get the ratio times 360.
+  var hrDeg = minDeg = secDeg = hrs = mins = 0;
+  if (seconds >= 3600) {
+    hrs = seconds/3600;
+    hrDeg = (hrs/12)*360;
+    if (hrDeg > 360) {//We only do this for hours because any minutes greater than sixty will be a part of the hours.
+      hrDeg = hrDeg%360;
+    }
+    seconds = seconds%3600
+  }
+  if (seconds >= 60) {
+    mins = seconds/60;
+    minDeg = (mins/60)*360;
+    seconds = seconds%60;
+  }
+  else {
+    secDeg = (seconds/60)*360;
+  }
+  hrDeg = hrDeg.toFixed(3)
+  minDeg = minDeg.toFixed(3)
+  secDeg = secDeg.toFixed(3)
+  console.log("Hour hand: "+hrDeg+" degs. Minute hand: "+minDeg+" degs. Second hand: "+secDeg+" degs.")
+}
+
+clockHandAngles(3600);
+clockHandAngles(119730);
+clockHandAngles(30)
