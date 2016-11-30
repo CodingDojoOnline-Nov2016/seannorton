94d41b479f90b345a273554d96a63484d97446be
Sean Norton <seannorton0807@gmail.com>
2016-11-30 08:20:16 -0600

    names.py optional assignment done, i think there could be a better way to do it tho...

--- pythonHW/names.py
+++ pythonHW/names.py
@@ -18,3 +18,39 @@ for i in range(len(students)-1):
 # for i in range(len(students)-1):
 #     for val in students[i].itervalues():
 #         print val
+
+#Optional Part
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+#Create a program that prints  the following format (including number of characters in each combined name):
+
+# Students
+# 1 - MICHAEL JORDAN - 13
+# 2 - JOHN ROSALES - 11
+# 3 - MARK GUILLEN - 11
+# 4 - KB TONEL - 7
+# Instructors
+# 1 - MICHAEL CHOI - 11
+# 2 - MARTIN PURYEAR - 13
+
+# print users['Students'][1]
+# print users['Instructors']
+
+for key in users.iterkeys():
+    print key
+    for i in range(len(users[key])):
+        print i+1, " - ", users[key][i]['first_name'], users[key][i]['last_name'], " - ", (len(users[key][i]['first_name']) + len(users[key][i]['last_name']))
+
+#i feel like there's a simpler way to do this without using 'firstname' and 'lastname'...
